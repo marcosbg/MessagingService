783b2a040876027e6ca5d268f74cd5678d7d75fb
Marcos Bezerra <marcos_bezerra@Marcoss-MacBook-Pro.local>
2022-12-23 14:24:29 -0300

    Add pipeline code

--- infra/src/main/java/com/myorg/InfraStack.java
+++ infra/src/main/java/com/myorg/InfraStack.java
@@ -2,6 +2,9 @@ package com.myorg;
 
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.pipelines.CodePipeline;
+import software.amazon.awscdk.pipelines.CodePipelineSource;
+import software.amazon.awscdk.pipelines.ShellStep;
 import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.Table;
@@ -11,6 +14,8 @@ import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+
+import java.util.List;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
 
@@ -41,6 +46,17 @@ public class InfraStack extends Stack {
                         .type(AttributeType.STRING)
                         .build())
                 .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+
+        CodePipeline.Builder.create(this, "Pipeline")
+                .pipelineName("MessageServicePipeline")
+                .synth(ShellStep.Builder.create("Synth")
+                        .input(CodePipelineSource.gitHub("marcosbg/MessagingService","main"))
+                        .commands(List.of(
+                                "mvn clean install",
+                                "cd infra",
+                                "cdk synth"))
+                        .build())
                 .build();
     }
 }
